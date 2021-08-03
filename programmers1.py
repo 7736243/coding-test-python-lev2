36eabde83626922e91d68e160862a69b6354ac52
7736243 <7736243@naver.com>
2021-08-03 13:47:34 +0900

    20210803 programmers 부족한 금액 계산하기

---
+++ programmers1.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+    answer = 0
+    
+    for i in range(1, count+1): 
+        answer += price*i 
+        
+    if money-answer >= 0:   
+        answer = 0
+    else:   
+        answer -= money 
+        
+    return answer
