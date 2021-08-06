bc72f01e880bcaa18bb4b34f524228f2fe4065f1
7736243 <7736243@naver.com>
2021-08-07 00:25:03 +0900

    20210807 baekjoon 11651

---
+++ baekjoon6.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+listt = []
+
+for i in range(n):
+    x, y = map(int, input().split())
+    listt.append([x, y])
+    
+listt.sort(key = lambda x : (x[1], x[0]))
+
+for i in range(n):
+    print(listt[i][0], listt[i][1])
