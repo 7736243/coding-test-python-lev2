58a484b5b6dead2085d8f3e3b329db90d8520a2b
7736243 <7736243@naver.com>
2021-08-09 00:31:35 +0900

    20210809 baekjoon 10845

---
+++ baekjoon13.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+queue = []
+
+for i in range(n):
+    a = input().split()
+    
+    if a[0]=="push":    queue.append(int(a[1]))
+    elif a[0]=="pop":
+        if len(queue)==0:    print("-1")
+        else:    print(queue.pop(0))
+    elif a[0]=="size":    print(len(queue))
+    elif a[0]=="empty":
+        if len(queue)==0:    print("1")
+        else:    print("0")
+    elif a[0]=="front":
+        if len(queue)==0:    print("-1")
+        else:    print(queue[0])
+    elif a[0]=="back":
+        if len(queue)==0:    print("-1")
+        else:    print(queue[len(queue)-1])
